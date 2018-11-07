e75b5cb7fdfae4667ef75d28b91472cae6c96992
Egor Lynko <flexoid@gmail.com>
2018-11-07 20:51:04 +0300

    Serve index page from any route for react-router

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,8 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  get "*path", to: "static_pages#index", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
