a12e5f2ce964dd7b9e20d5da453bb6dbf87b3694
Egor Lynko <flexoid@gmail.com>
2018-11-07 22:18:41 +0300

    Live test case counter update

--- client/src/reducers/entities.js
+++ client/src/reducers/entities.js
@@ -15,6 +15,25 @@ import merge from 'lodash/merge'
 const entitiesById = (data) =>
   data.reduce((acc, entity) => ({ ...acc, [entity.id]: entity }), {})
 
+const testCaseUpdated = (state, action) => {
+  let testCase = action.data
+  let stateChange = { testCases: { [action.data.id]: testCase } }
+
+  // Not a new test case, no need to update counter.
+  if (state.testCases[testCase.id]) { return stateChange }
+
+  let feature = state.features[testCase.feature_id]
+  if (!feature) { return stateChange }
+
+  let testRun = state.testRuns[feature.test_run_id]
+  if (!testRun) { return stateChange }
+
+  let testCasesCount = (testRun.test_cases_count || 0) + 1
+  stateChange.testRuns = { [testRun.id]: { test_cases_count: testCasesCount } }
+
+  return stateChange
+}
+
 const entities = (state = { projects: {}, testRuns: {}, features: {}, testCases: {}, testSteps: {} }, action) => {
   switch (action.type) {
     case FETCH_PROJECTS_SUCCESS:
@@ -31,7 +50,7 @@ const entities = (state = { projects: {}, testRuns: {}, features: {}, testCases:
     case FETCH_TEST_CASES_SUCCESS:
       return merge({}, state, { testCases: entitiesById(action.data) })
     case TEST_CASE_UPDATED:
-      return merge({}, state, { testCases: { [action.data.id]: action.data } })
+      return merge({}, state, testCaseUpdated(state, action))
     case FETCH_TEST_STEPS_SUCCESS:
       return merge({}, state, { testSteps: entitiesById(action.data) })
     case TEST_STEP_UPDATED:
